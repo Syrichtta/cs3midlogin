4d5d589b50fa97e301b8d7c721a89ddab7244353
Syrichtta <ethcalio@gmail.com>
2024-10-11 13:56:56 +0800

    add elevation

--- lib/src/screens/profile.dart
+++ lib/src/screens/profile.dart
@@ -45,145 +45,154 @@ class _ProfileScreenState extends State<ProfileScreen> {
         color: const Color(0xFF6218AB),
         child: profileData == null
             ? const Center(child: CircularProgressIndicator())
-            : Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/home');
-                          },
-                          child: Icon(
-                            Icons.arrow_back,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/');
-                          },
-                          child: Icon(
-                            Icons.logout,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Stack(
-                        fit: StackFit.expand,
+            : SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Positioned(
-                            top: 100,
-                            left: 0,
-                            right: 0,
-                            child: Card(
-                              elevation: 4,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Padding(
-                                padding: EdgeInsets.all(16.0),
-                                child: Column(
-                                  children: [
-                                    SizedBox(
-                                      height: 100,
-                                    ),
-                                    Text(
-                                      '${profileData!['name']['title']}. ${profileData!['name']['first']} ${profileData!['name']['last']}',
-                                      style: TextStyle(
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.w700),
-                                    ),
-                                    SizedBox(
-                                      height: 25,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Contact Details',
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w500),
-                                        )
-                                      ],
-                                    ),
-                                    Divider(
-                                      color: Colors.grey,
-                                    ),
-                                    IconTextPair(
-                                        icon: Icons.email,
-                                        text: '${profileData!['email']}'),
-                                    IconTextPair(
-                                        icon: Icons.phone,
-                                        text: '${profileData!['phone']}'),
-                                    IconTextPair(
-                                        icon: Icons.phone_android,
-                                        text: '${profileData!['cell']}'),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Personal Details',
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w500),
-                                        )
-                                      ],
-                                    ),
-                                    Divider(
-                                      color: Colors.grey,
-                                    ),
-                                    IconTextPair(
-                                        icon: Icons.calendar_today,
-                                        text:
-                                            '$formattedDob | ${profileData!['dob']['age']} years old'),
-                                    IconTextPair(
-                                        icon: profileData!['gender'] == 'male'
-                                            ? Icons.male
-                                            : profileData!['gender'] == 'female'
-                                                ? Icons.female
-                                                : Icons.person,
-                                        text: '${profileData!['gender']}'),
-                                    IconTextPair(
-                                        icon: Icons.location_on,
-                                        text:
-                                            '${profileData!['location']['street']['number']} ${profileData!['location']['street']['name']}, ${profileData!['location']['city']},  ${profileData!['location']['state']}, ${profileData!['location']['country']}, ${profileData!['location']['postcode']}')
-                                  ],
-                                ),
-                              ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/home');
+                            },
+                            child: Icon(
+                              Icons.arrow_back,
+                              size: 25,
+                              color: Colors.white,
                             ),
                           ),
-                          Align(
-                            alignment: Alignment.topCenter,
-                            child: Container(
-                              width: 200,
-                              height: 200,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Color(0xFF6218AB),
-                                  width: 10,
-                                ),
-                              ),
-                              child: CircleAvatar(
-                                radius: 100,
-                                backgroundImage: NetworkImage(
-                                    profileData!['picture']['large']),
-                              ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/');
+                            },
+                            child: Icon(
+                              Icons.logout,
+                              size: 25,
+                              color: Colors.white,
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              top: 100,
+                              left: 0,
+                              right: 0,
+                              child: Card(
+                                elevation: 4,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Column(
+                                    children: [
+                                      SizedBox(
+                                        height: 100,
+                                      ),
+                                      Text(
+                                        '${profileData!['name']['title']}. ${profileData!['name']['first']} ${profileData!['name']['last']}',
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.w700),
+                                      ),
+                                      SizedBox(
+                                        height: 25,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Contact Details',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(
+                                        color: Colors.grey,
+                                      ),
+                                      IconTextPair(
+                                          icon: Icons.email,
+                                          text: '${profileData!['email']}'),
+                                      IconTextPair(
+                                          icon: Icons.phone,
+                                          text: '${profileData!['phone']}'),
+                                      IconTextPair(
+                                          icon: Icons.phone_android,
+                                          text: '${profileData!['cell']}'),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Personal Details',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(
+                                        color: Colors.grey,
+                                      ),
+                                      IconTextPair(
+                                          icon: Icons.calendar_today,
+                                          text:
+                                              '$formattedDob | ${profileData!['dob']['age']} years old'),
+                                      IconTextPair(
+                                          icon: profileData!['gender'] == 'male'
+                                              ? Icons.male
+                                              : profileData!['gender'] ==
+                                                      'female'
+                                                  ? Icons.female
+                                                  : Icons.person,
+                                          text: '${profileData!['gender']}'),
+                                      IconTextPair(
+                                          icon: Icons.location_on,
+                                          text:
+                                              '${profileData!['location']['street']['number']} ${profileData!['location']['street']['name']}, ${profileData!['location']['city']},  ${profileData!['location']['state']}, ${profileData!['location']['country']}, ${profileData!['location']['postcode']}')
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Container(
+                                width: 200,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Color(0xFF6218AB),
+                                    width: 10,
+                                  ),
+                                ),
+                                child: Material(
+                                  elevation:
+                                      10, // Adjust the elevation as needed
+                                  shape:
+                                      CircleBorder(), // Ensures the shadow is circular
+                                  child: CircleAvatar(
+                                    radius: 100,
+                                    backgroundImage: NetworkImage(
+                                        profileData!['picture']['large']),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
       ),
